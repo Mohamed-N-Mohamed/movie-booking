662f4f8d098810df9ad41e33abfd31c5ae16099f
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2021-04-05 17:51:45 +0100

    added local storage / updated score + seats

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//DOM Element
 const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
@@ -5,11 +6,48 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 let ticketPrice = +movieSelect.value;
 
+//update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   const selectedSeatCount = selectedSeats.length;
   count.innerText = selectedSeatCount;
   total.innerText = selectedSeatCount * ticketPrice;
+
+  const seatIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+
+  //store to local storage
+  localStorage.setItem("selectedSeats", JSON.stringify(seatIndex));
+}
+
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMovieprice", moviePrice);
+}
+
+//display movies from local storage
+function createUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if(selectedSeats !== null && selectedSeats.length > 0){
+    seats.forEach((seat, index) => {
+      if(selectedSeats.indexOf(index) > -1){
+        seat.classList.add('selected')
+      }
+   
+
+    })
+
+  }
+
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if(selectedMovieIndex !== null){
+    movieSelect.selectedIndex = selectedMovieIndex 
+  }
 }
 
 container.addEventListener("click", (e) => {
@@ -22,7 +60,14 @@ container.addEventListener("click", (e) => {
   }
 });
 
+//movie select event
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
+
+
+//init 
+createUI()
+updateSelectedCount()
